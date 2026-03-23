5dd86a17f64df0c19e80b6ebe8181bcd8068e97f
Valli-Veeravel <118924746+Valli-Veeravel@users.noreply.github.com>
2026-03-23 02:25:03 -0700

    Assignment 4 changes

--- src/rps/ComputerStrategy.java
+++ src/rps/ComputerStrategy.java
@@ -2,4 +2,12 @@ package rps;
 
 public interface ComputerStrategy {
     Move chooseMove(int roundNumber);
+
+    default void observeRound(Move humanMove, Move computerMove) {
+        // Default no-op for strategies that do not learn.
+    }
+
+    default void onGameOver() {
+        // Default no-op for strategies without persistent state.
+    }
 }
